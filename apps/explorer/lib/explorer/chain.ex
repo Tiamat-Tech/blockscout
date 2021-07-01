af8fdfc2076cc9673e6b41ef11323e835caddcf5
nikitosing <nikitosing4@mail.ru>
2021-07-01 16:14:58 +0700

    add clause

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -3706,8 +3706,8 @@ defmodule Explorer.Chain do
     end
   end
 
-  def smart_contract_full_verified?(address) when is_binary(address) do
-    case string_to_address_hash(address) do
+  def smart_contract_full_verified?(address_hash_str) when is_binary(address_hash_str) do
+    case string_to_address_hash(address_hash_str) do
       {:ok, address_hash} ->
         check_full_verified(address_hash)
 
@@ -3716,8 +3716,8 @@ defmodule Explorer.Chain do
     end
   end
 
-  def smart_contract_full_verified?(address) do
-    check_full_verified(address)
+  def smart_contract_full_verified?(address_hash) do
+    check_full_verified(address_hash)
   end
 
   defp check_full_verified(address_hash) do
@@ -3732,20 +3732,28 @@ defmodule Explorer.Chain do
     if result, do: !result.partial
   end
 
-  def smart_contract_verified?(address_hash_str) do
+  def smart_contract_verified?(address_hash_str) when is_binary(address_hash_str) do
     case string_to_address_hash(address_hash_str) do
       {:ok, address_hash} ->
-        query =
-          from(
-            smart_contract in SmartContract,
-            where: smart_contract.address_hash == ^address_hash
-          )
-
-        if Repo.one(query), do: true, else: false
+        check_verified(address_hash)
 
       _ ->
         false
     end
+  end
+
+  def smart_contract_verified?(address_hash) do
+    check_verified(address_hash)
+  end
+
+  defp check_verified(address_hash) do
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    if Repo.one(query), do: true, else: false
   end
 
   defp fetch_transactions(paging_options \\ nil) do
